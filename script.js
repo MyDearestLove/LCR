c2ceb8ce5624ff90bf7088a17980d3962d3d4ac4
MyDearestLove <155743596+MyDearestLove@users.noreply.github.com>
2024-01-05 09:38:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+    Charles Smith
+    File Name: script.js
+    Date: 27 November, 2023
+*/
+
+//jQuery for hero image to consume the header window space
+	$(document).ready(function(){
+	$('.hero').height($(window).height());
+	
+});
